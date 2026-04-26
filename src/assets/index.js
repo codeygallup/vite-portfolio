798426c1158255f1a573b2aee7cc019c5394ee26
Codeyg12 <codey.gallup@gmail.com>
2026-04-26 16:48:09 -0600

    New project images and gifs

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,5 +7,11 @@ import Employee from "./mysqletrack.png";
 import Hangman from "./starhang.png";
 import Book from "./booksearch.png";
 import Weather from './weather.png'
+import Fatcat from "./fatcat.png";
+import OnTaskGif from "./ontask.gif"
+import QuikLogVSC from "./vscode-quiklog.gif";
+import QuikLogJB from "./jetbrains-quiklog.gif";
+import QuikLogJBMarket from "./qljb-market.png"
+import QuikLogVSCMarket from "./qlvsc-market.png"
 
-export { ORM, Jate, TechTalk, Scribe, OnTask, Employee, Hangman, Book, Weather };
+export { ORM, Jate, TechTalk, Scribe, OnTask, Employee, Hangman, Book, Weather, Fatcat, OnTaskGif, QuikLogVSC, QuikLogJB, QuikLogJBMarket, QuikLogVSCMarket };
