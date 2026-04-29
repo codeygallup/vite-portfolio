6a4c276ed34646d89bdc58499fa37a6b96256114
Codeyg12 <codey.gallup@gmail.com>
2026-04-29 11:11:26 -0600

    Updating techlist for new format

--- src/data/technology-list.js
+++ src/data/technology-list.js
@@ -1,58 +1,27 @@
-export const techList = [
+export const techCategories = [
   {
-    title: "HTML",
-    url: "https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
+    label: "Languages",
+    skills: ["JavaScript", "TypeScript", "Java", "Python", "Kotlin"],
+    icons: ["js", "ts", "java", "py", "kotlin"],
   },
   {
-    title: "CSS",
-    url: "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+    label: "Tools",
+    skills: ["Git", "GitHub", "Grafana", "Postman", "VS Code", "IntelliJ IDEA", "Jira"],
+    icons: ["git", "github", "grafana", "postman", "vscode", "idea", "jira"],
   },
   {
-    title: "JavaScript",
-    url: "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+    label: "Databases",
+    skills: ["MySQL", "MongoDB", "PostgreSQL"],
+    icons: ["mysql", "mongodb", "postgres"],
   },
   {
-    title: "jQuery",
-    url: "https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white",
+    label: "Testing & CI/CD",
+    skills: ["Jenkins", "Gherkin", "Karate"],
+    icons: ["jenkins", "gherkin", "karate"],
   },
   {
-    title: "Node.js",
-    url: "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
-  },
-  {
-    title: "Express",
-    url: "https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB",
-  },
-  {
-    title: "MySQL",
-    url: "https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white",
-  },
-  {
-    title: "MongoDB",
-    url: "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
-  },
-  {
-    title: "Apollo GraphQL",
-    url: "https://img.shields.io/badge/-ApolloGraphQL-311C87?style=for-the-badge&logo=apollo-graphql",
-  },
-  {
-    title: "React",
-    url: "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
-  },
-  {
-    title: "Python",
-    url: "https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54",
-  },
-  {
-    title: "Django",
-    url: "https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white",
-  },
-  {
-    title: "Java",
-    url: "https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white",
-  },
-  {
-    title: "Tailwind",
-    url: "https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white",
+    label: "Frameworks & Libraries",
+    skills: ["React", "Tailwind CSS", "Vite", "Node.js", "Express", "Spring Boot", "Vue 3"],
+    icons: ["react", "tailwind", "vite", "nodejs", "express", "spring", "vue"],
   },
 ];
