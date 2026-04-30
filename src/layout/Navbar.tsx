9e56098196bd0f844fc98b15780294bbc8a32e5c
Codeyg12 <codey.gallup@gmail.com>
2026-04-29 18:43:14 -0600

    Improving lighthouse score

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -13,6 +13,7 @@ export default function Navbar() {
     <>
       <nav className="nav-bar fixed top-0 z-10 min-h-28 w-screen bg-cyan-600 text-3xl text-slate-200 md:text-4xl lg:text-4xl">
         <NavLink
+          aria-label="About page"
           end
           className={({ isActive }) =>
             `about-link absolute left-[15%] top-9 sm:left-[25%] ${isActive ? "active" : ""}`
@@ -22,6 +23,7 @@ export default function Navbar() {
           <FontAwesomeIcon icon={faUser} />
         </NavLink>
         <NavLink
+          aria-label="Skills page"
           end
           className={({ isActive }) =>
             `skill-link absolute left-[30%] top-9 sm:left-[40%] ${isActive ? "active" : ""}`
@@ -31,6 +33,7 @@ export default function Navbar() {
           <FontAwesomeIcon icon={faCode} />
         </NavLink>
         <NavLink
+          aria-label="Projects page"
           end
           className={({ isActive }) =>
             `project-link absolute left-[50%] top-9 sm:left-[55%] ${isActive ? "active" : ""}`
@@ -40,6 +43,7 @@ export default function Navbar() {
           <FontAwesomeIcon icon={faFolderOpen} />
         </NavLink>
         <NavLink
+          aria-label="Contact page"
           end
           className={({ isActive }) =>
             `contact-link absolute left-[70%] top-9 ${isActive ? "active" : ""}`
@@ -49,22 +53,28 @@ export default function Navbar() {
           <FontAwesomeIcon icon={faEnvelopeOpenText} />
         </NavLink>
         <ul className="sidebar-list text-2xl text-slate-200">
-          <a
-            href="https://github.com/codeygallup"
-            rel="noreferrer"
-            target="_blank"
-            className="ga-icon absolute right-4 md:right-8 top-3 "
-          >
-            <FontAwesomeIcon icon={faGithubSquare} />
-          </a>
-          <a
-            href="https://linkedin.com/in/codey-gallup"
-            rel="noreferrer"
-            target="_blank"
-            className="li-icon absolute right-4 md:right-8 top-14 sm:right-12"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
+          <li>
+            <a
+              aria-label="Codey Gallup GitHub"
+              href="https://github.com/codeygallup"
+              rel="noreferrer"
+              target="_blank"
+              className="ga-icon absolute right-4 top-3 md:right-8"
+            >
+              <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+          </li>
+          <li>
+            <a
+              aria-label="Codey Gallup LinkedIn"
+              href="https://linkedin.com/in/codey-gallup"
+              rel="noreferrer"
+              target="_blank"
+              className="li-icon absolute right-4 top-14 sm:right-12 md:right-8"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
         </ul>
       </nav>
     </>
